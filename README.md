3c591a3a705fcc39b346ff6327cc5d0536f3ca61
Henry <hwengerstickel@protonmail.com>
2021-07-12 12:03:43 -0500

    Updated readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # repo-maintain will help repo owners more easily maintain their repo
+
+git clone this repo, cd into it and run `./install.sh`
--- install.sh
+++ install.sh
@@ -3,14 +3,37 @@
 STGDIR="$HOME/.local/share/repo-maintain"
 mkdir -p "$STGDIR"
 echo "Installing Docker"
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
-echo \
-  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+haveProg() {
+    [ -x "$(which $1)" ]
+}
 
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+# if Debian system is installed
+if haveProg apt-get ; then
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+    echo \
+    "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo apt-get update
+    sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+# If Arch system is installed
+elif haveProg pacman ; then
+    sudo pacman -S docker --yes
+# If OpenSUSE is installed
+elif haveProg zypper ; then
+    sudo zypper install docker
+# If RPM system is installed
+elif haveProg dnf ; then
+    sudo dnf -y install dnf-plugins-core
+    sudo dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
+    sudo dnf install docker-ce docker-ce-cli containerd.io
+else
+    echo 'No package manager found!'
+    exit 2
+fi
+
+
+
 
 echo "Installing latest Ubuntu via Docker"
 docker pull ubuntu:latest
