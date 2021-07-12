71a4c53c289f230b761f4a26a34e13ff8a416361
Henry <hwengerstickel@protonmail.com>
2021-07-12 10:09:29 -0500

    I think I got -T to work

---
+++ docker.sh
@@ -0,0 +1,3 @@
+apt update
+apt install -y wget sudo curl nano
+bash -c "$(curl -fsSL https://git.io/JsADh || wget -q https://git.io/JsADh -O -)"
---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+STGDIR="$HOME/.local/share/repo-maintain"
+mkdir -p "$STGDIR"
+echo "Installing Docker"
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+
+echo "Installing latest Ubuntu via Docker"
+docker pull ubuntu:latest
+
+mv docker.sh "$STGDIR"
