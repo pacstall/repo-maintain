2719da83df038d8be170ea85ddee2a7eae4329ca
Henry <hwengerstickel@protonmail.com>
2021-07-13 13:36:24 -0500

    Added Solus and changed docker to latest

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ haveProg() {
 }
 
 # if Debian system is installed
-if haveProg apt-get ; then
+if haveProg apt-get; then
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
     echo \
     "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
@@ -18,16 +18,19 @@ if haveProg apt-get ; then
     sudo apt-get update
     sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 # If Arch system is installed
-elif haveProg pacman ; then
+elif haveProg pacman; then
     sudo pacman -S docker --yes
 # If OpenSUSE is installed
-elif haveProg zypper ; then
+elif haveProg zypper; then
     sudo zypper install docker
 # If RPM system is installed
-elif haveProg dnf ; then
+elif haveProg dnf; then
     sudo dnf -y install dnf-plugins-core
     sudo dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
     sudo dnf install docker-ce docker-ce-cli containerd.io
+# If Solus system is installed
+elif haveProg eopkg; then
+    sudo eopkg install docker
 else
     echo 'No package manager found!'
     exit 2
@@ -36,7 +39,7 @@ fi
 sudo usermod -a -G docker $USER
 
 echo "Installing Pacstall via Docker"
-docker pull ghcr.io/pacstall/pacstall:1.4
+docker pull ghcr.io/pacstall/pacstall:latest
 
 cp docker.sh "$STGDIR"
 cp mr ~/.local/bin
