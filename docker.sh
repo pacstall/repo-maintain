24df32f9a96532220b3f031f6a84dc46dcaa24ad
NarutoXY <79555780+NarutoXY@users.noreply.github.com>
2021-07-18 15:59:36 +0530

    Suppress errors and use -y

--- docker.sh
+++ docker.sh
@@ -1,4 +1,4 @@
 # This is the default script that will setup pacstall in a next to nothing, barebones container. Unless you know what your doing, don't mess with anything in this file
-apt update
+apt update -y 2>/dev/null
 # apt install -y wget sudo curl git nano zip unzip stow
 # bash -c "$(curl -fsSL https://git.io/JsADh || wget -q https://git.io/JsADh -O -)"
